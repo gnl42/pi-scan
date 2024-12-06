07b9bad1eb585708b8d24b6eb406f7f047c1edef
George Lindholm <66577321+gnl42@users.noreply.github.com>
2024-12-05 22:53:07 -0800

    More experiments with packages/modules

--- piscan-2/04-install-setup/01-run-chroot.sh
+++ piscan-2/04-install-setup/01-run-chroot.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -e
-
+export PIP_ROOT_USER_ACTION=ignore
 # Pi Scan
-python3 --version
+
+# Default pip is painfully slow
 pip install pip==24.3.1 --upgrade
-pip --version
+
 pip install resolvelib==1.1.0
 pip install ninja==1.11.1.2
 pip install patchelf==0.17.2.1
@@ -11,8 +12,13 @@ pip install numpy==1.26.4
 pip install dbus-python==1.3.2
 pip install six
 pip install wiringpi==2.60.1
-pip install kivy[full]
+#pip install -U pysdl2
+#pip install pangoft2
+pip install olefile
+pip install kivy
+
 mkdir -p /home/pi/pi-scan
+
 # TODO when this gets moved into pi-scan repo this should be replaced
 git clone --depth 1 https://github.com/gnl42/pi-scan-tenrec /tmp/pi-scan
 sudo cp -r /tmp/pi-scan/src/*                 /home/pi/pi-scan/
@@ -21,14 +27,24 @@ sudo cp -r /tmp/pi-scan/resources/spinner.gif /home/pi/pi-scan/
 # For Touch Screen
 mkdir -p /home/pi/.kivy
 
-#pip install lupa --config-settings='--no-luajit'
-#pip install chdkptp.py==0.1.3
-# chdkptp.py
 cd /home/pi
+pip install -I Cython
+
+#pip install lupa --install-option='--no-luajit'
+# --install-option was removed from pip with 23.0.1 so do it manually
+git clone --depth 1 --recursive --branch lupa-2.2 https://github.com/scoder/lupa.git
+cd lupa
+python3 setup.py install bdist_wheel --no-bundle --with-cython
+cd ..
+
+# chdkptp.py
 git clone --depth 1 https://github.com/5up3rD4n1/chdkptp.py
 cd chdkptp.py
 git submodule init
 git submodule update
 python setup.py install
 #
-chown -R pi:pi /home/pi
+#chown -R pi:pi /home/pi
+cd /home/pi/pi-scan
+gphoto2 --version || true
+python3 main.py || true
